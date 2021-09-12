6d05d90ee7698fbe2b102ac239ec4e783a666082
Felipe Jacinto <74006952+DS-Felipe-Jacinto@users.noreply.github.com>
2021-09-12 18:38:18 -0300

    Add files via upload

---
+++ Revisão JOIN, PROCEDURE, CÁLCULOS - BD (09-09-2021)/Arquivos (.sql)/bdVendas.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE bdVendas
+USE bdVendas
+
+CREATE TABLE tbl_Vendas(
+Mes VARCHAR (100)
+,Produtos VARCHAR (100)
+,Vendedor VARCHAR (100)
+,Vendas FLOAT
+,Regiao VARCHAR (100)
+);
+
+INSERT INTO tbl_Vendas(Mes, Produtos, Vendedor, Vendas, Regiao)
+VALUES ('Maio','Latricínios','Amaral',5477.00,'Oeste')
+	,('Maio','Carnes','Bomfim',8012.00,'Leste')
+	,('Maio','Latricínios','Amaral',9411.00,'Oeste')
+	,('Maio','Latricínios','Amaral',8089.00,'Sul')
+	,('Maio','Carnes','Bomfim',9566.00,'Sul')
+	,('Junho','Latricínios','Amaral',6805.00,'Leste')
+	,('Junho','Carnes','Bomfim',265.00,'Sul')
+	,('Junho','Carnes','Bomfim',5575.00,'Leste')
+	,('Junho','Carnes','Amaral',5971.00,'Sul')
+	,('Junho','Latricínios','Bomfim',4046.00,'Norte')
+	,('Junho','Latricínios','Bomfim',6085.00,'Oeste')
+	,('Junho','Carnes','Bomfim',1361.00,'Norte')
+
+/* Imprimir o valor total das vendas do mês de Maio e Junho */
+SELECT SUM(Vendas) AS 'Total'
+FROM tbl_Vendas
+
+SELECT COUNT(Vendas) AS 'N° de Vendas (Leste)'
+FROM tbl_Vendas 
+WHERE Regiao = 'Leste'
+
+SELECT AVG(Vendas) AS 'Média de Vendas (Maio)'
+FROM tbl_Vendas 
+WHERE Mes = 'Maio'
+
+SELECT SUM(Vendas) * 0.20 AS 'Total (+20%)'
+FROM tbl_Vendas
